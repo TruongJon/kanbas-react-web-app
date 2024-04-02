e73b2fabb528fed4d7868c3b5c01d26d63ae04a0
Jonathan Truong <truong.jo@northeastern.edu>
2024-04-02 19:19:35 -0400

    Implement HTTP server for a5

--- src/Kanbas/index.tsx
+++ src/Kanbas/index.tsx
@@ -3,30 +3,47 @@ import KanbasNavigation from "./Navigation";
 import Dashboard from "./Dashboard";
 import Courses from "./Courses";
 import { courses as db } from "./Databases";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { Provider } from "react-redux";
 import store from "./store";
+import axios from "axios";
 
 function Kanbas() {
-  const [courses, setCourses] = useState<any[]>(db);
+  const [courses, setCourses] = useState<any[]>([]);
   const [course, setCourse] = useState({
     _id: "1234", name: "New Course", number: "New Number",
     startDate: "2023-09-10", endDate: "2023-12-15",
   });
-  const addNewCourse = () => {
-    setCourses([...courses, { ...course, _id: new Date().getTime().toString() }]);
+  const COURSES_API = "http://localhost:4000/api/courses";
+  const findAllCourses = async () => {
+    const response = await axios.get(COURSES_API);
+    setCourses(response.data);
   };
-  const deleteCourse = (courseId: any) => {
-    setCourses(courses.filter((course) => course._id !== courseId));
+  useEffect(() => {
+    findAllCourses();
+  }, []);
+  const addNewCourse = async () => {
+    const response = await axios.post(COURSES_API, course);
+    setCourses([ ...courses, response.data ]);
   };
-  const updateCourse = () => {
+  const deleteCourse = async (courseId: string) => {
+    const response = await axios.delete(
+      `${COURSES_API}/${courseId}`
+    );
+    setCourses(courses.filter(
+      (c) => c._id !== courseId));
+  };
+  const updateCourse = async () => {
+    const response = await axios.put(
+      `${COURSES_API}/${course._id}`,
+      course
+    );
     setCourses(
       courses.map((c) => {
         if (c._id === course._id) {
           return course;
-        } else {
-          return c;
         }
+        return c;
       })
     );
   };
---
+++ src/Labs/a5/WorkingWithObjects.js
@@ -0,0 +1,109 @@
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+function WorkingWithObjects() {
+    const [assignment, setAssignment] = useState({
+        id: 1, title: "NodeJS Assignment",
+        description: "Create a NodeJS server with ExpressJS",
+        due: "2021-10-10", completed: false, score: 0,
+    });
+    const ASSIGNMENT_URL = "http://localhost:4000/a5/assignment"
+    const [module, setModule] = useState({
+        id: 1, name: "HTML",
+        description: "Learn HTML + CSS",
+        course: "CS4550",
+    });
+    const MODULE_URL = "http://localhost:4000/a5/module"
+    const fetchAssignment = async () => {
+        const response = await axios.get(`${ASSIGNMENT_URL}`);
+        setAssignment(response.data);
+    };
+    const updateTitle = async () => {
+        const response = await axios
+            .get(`${ASSIGNMENT_URL}/title/${assignment.title}`);
+        setAssignment(response.data);
+    };
+    useEffect(() => {
+        fetchAssignment();
+    }, []);    
+    return (
+        <div>
+        <h3>Working With Objects</h3>
+        <h4>Modifying Properties</h4>
+        <button onClick={updateTitle} 
+            className="btn btn-primary me-2">
+            Update Title to: {assignment.title}
+        </button>
+        <button onClick={fetchAssignment} 
+            className="btn btn-primary me-2">
+            Fetch Assignment
+        </button>
+        <input onChange={(e) => setAssignment({
+            ...assignment, title: e.target.value })}
+        value={assignment.title} type="text" />
+        <br/>
+        <a href={`${ASSIGNMENT_URL}/title/${assignment.title}`}
+            className="btn btn-primary me-2">
+            Update Title
+        </a>
+        <input type="text" 
+            onChange={(e) => setAssignment({ ...assignment,
+                title: e.target.value })}
+            value={assignment.title}/>
+        <br/>
+        <a href={`${ASSIGNMENT_URL}/score/${assignment.score}`}
+            className="btn btn-primary me-2">
+            Update Score
+        </a>
+        <input type="number" 
+            onChange={(e) => setAssignment({ ...assignment,
+                score: e.target.value })}
+            value={assignment.score}/>
+        <br/>
+        <a href={`${ASSIGNMENT_URL}/completed/${assignment.completed}`}
+            className="btn btn-primary me-2">
+            Update Completed
+        </a>
+        <input type="checkbox" 
+            onChange={(e) => setAssignment({ ...assignment,
+                completed: e.target.checked })}
+            checked={assignment.completed}/>
+        <br/>
+        <a href={`${MODULE_URL}/name/${module.name}`}
+            className="btn btn-primary me-2">
+            Update Module Name
+        </a>
+        <input type="text" 
+            onChange={(e) => setModule({ ...module,
+                name: e.target.value })}
+            value={module.name}/>
+        <br/>
+        <a href={`${MODULE_URL}/description/${module.description}`}
+            className="btn btn-primary me-2">
+            Update Module Description
+        </a>
+        <input type="text" 
+            onChange={(e) => setModule({ ...module,
+                description: e.target.value })}
+            value={module.description}/>
+        <h4>Retrieving Objects</h4>
+        <a href="http://localhost:4000/a5/assignment"
+            className="btn btn-primary me-2">
+            Get Assignment
+        </a>
+        <a href="http://localhost:4000/a5/module"
+            className="btn btn-primary me-2">
+            Get Module
+        </a>
+        <h4>Retrieving Properties</h4>
+        <a href="http://localhost:4000/a5/assignment/title"
+            className="btn btn-primary me-2">
+            Get Title
+        </a>
+        <a href="http://localhost:4000/a5/module/name"
+            className="btn btn-primary me-2">
+            Get Module Name
+        </a>
+        </div>
+    );
+}
+export default WorkingWithObjects;
